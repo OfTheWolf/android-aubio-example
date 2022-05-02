6bc591cdba3c9ab7e69857f965ea912cf66909ce
ugur <uguboz@gmail.com>
2022-05-02 16:21:00 +0100

    update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,11 +2,21 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include $(CLEAR_VARS)
 LOCAL_MODULE := aubio
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libaubio.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-include $(PREBUILT_SHARED_LIBRARY)
+APP_ABI := all
+
+#traverse all the directory and subdirectory
+define walk
+  $(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e)))
+endef
+
+#find all the file recursively under jni/
+ALLFILES = $(call walk, $(LOCAL_PATH))
+FILE_LIST := $(filter %.c, $(ALLFILES))
+LOCAL_CFLAGS += -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pitch
